3c4d80b968283fe4aa0008fd32d78768bda8520b
Pulkit Joshi <pulkitjoshi19x@gmail.com>
2025-06-22 12:16:42 +0530

    fetch data using Apollo useQuery and generate schema types with codegen

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache()
+})
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ApolloProvider client={client}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ApolloProvider>,
 )
